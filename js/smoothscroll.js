d836f9246bf63ca3f854ac83fe43ff2ee17256af
Ronnie <itsjustronnie@yahoo.com>
2015-01-07 20:12:06 -0800

    Added smooth scrolling

---
+++ js/smoothscroll.js
@@ -0,0 +1,31 @@
+$(".navbar-collapse ul li a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(this.hash).offset().top - 100
+     }, 300, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
+
+$("a.navbar-brand[href^='#']").on('click', function(e) {
+
+  e.preventDefault();
+
+  var hash = this.hash;
+  $('html, body').animate({
+    scrollTop: $(this.hash).offset().top
+  }, 300, function(){
+    window.location.hash = hash;
+  });
+});
